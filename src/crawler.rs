4d3c06398f854acc25555fbb43627642e5089436
Markus Ineichen <kusi.ineichen@gmail.com>
2021-03-03 02:36:05 +0100

    run cargo fmt

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,12 +1,23 @@
-//! Lacking of a good/maintained crate alternative. 
+//! Lacking of a good/maintained crate alternative.
 //! - Quick_crawler:0.1.2: the best candidate found, still very young ( during evaluation), no recursion support, aborts on error
 //! - url-crawler:0.3.0: uses threads. While usable for a few users, it requires too many OS threads to be used within a web api.                     
 
-use std::{collections::HashSet, marker::PhantomData, pin::Pin, sync::Arc, task::{Context, Poll}};
-use futures::{prelude::*, Stream, future::LocalBoxFuture};
-use actix_web::{error::PayloadError, web::{Buf, Bytes}};
-use scraper::{Html, Selector};
-use url::Url;
+use {
+    actix_web::{
+        error::PayloadError,
+        web::{Buf, Bytes},
+    },
+    futures::{future::LocalBoxFuture, prelude::*, Stream},
+    scraper::{Html, Selector},
+    std::{
+        collections::HashSet,
+        marker::PhantomData,
+        pin::Pin,
+        sync::Arc,
+        task::{Context, Poll},
+    },
+    url::Url,
+};
 
 // Factory to create new jobs for the crawler.
 trait CrawlJobFactory: Send + Sync + 'static {
@@ -16,10 +27,10 @@ trait CrawlJobFactory: Send + Sync + 'static {
 impl<F: Send + Sync + 'static, Fut> CrawlJobFactory for F
 where
     F: Fn(Url) -> Fut,
-    Fut: Future<Output=CrawlerStreamResult> + Send + 'static,
+    Fut: Future<Output = CrawlerStreamResult> + Send + 'static,
 {
     fn call<'a>(&'a self, url: Url) -> CrawlJob {
-        ((self)(url)).boxed_local()     
+        ((self)(url)).boxed_local()
     }
 }
 
@@ -31,7 +42,7 @@ pub struct CrawlerStreamResult {
     #[serde(serialize_with = "serialize_url_as_str")]
     pub url: Url,
     pub status_code: Option<u16>,
-    /// Found links on a page which will be followed by the 
+    /// Found links on a page which will be followed by the
     /// `CrawlerStream` if it was not parsed already.
     pub crawl_candidates: Vec<Url>,
 
@@ -39,9 +50,12 @@ pub struct CrawlerStreamResult {
     pub problems: Vec<PageProblem>,
 
     #[serde(skip)]
-    make_ctor_private: PhantomData<()>
+    make_ctor_private: PhantomData<()>,
 }
-fn serialize_url_as_str<S>(input: &Url, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
+fn serialize_url_as_str<S>(input: &Url, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
     serializer.serialize_str(input.as_str())
 }
 
@@ -49,7 +63,7 @@ impl CrawlerStreamResult {
     pub fn is_ok(&self) -> bool {
         match self.status_code {
             Some(v) => v < 300 && v >= 200,
-            None => false
+            None => false,
         }
     }
     fn new_error(url: Url, status_code: Option<u16>, problems: Vec<PageProblem>) -> Self {
@@ -58,14 +72,14 @@ impl CrawlerStreamResult {
             status_code,
             crawl_candidates: Vec::new(),
             problems,
-            make_ctor_private: PhantomData
+            make_ctor_private: PhantomData,
         }
     }
 }
 
 struct ParserResult {
     crawl_candidates: Vec<Url>,
-    problems: Vec<PageProblem>
+    problems: Vec<PageProblem>,
 }
 
 /// Possible weaknesses detected within a page.
@@ -74,9 +88,9 @@ struct ParserResult {
 #[derive(Debug, PartialEq, Eq, serde::Serialize)]
 pub enum PageProblem {
     #[non_exhaustive]
-    InsecureLink { url : Url },
+    InsecureLink { url: Url },
     /// CrawlJob returned a non successful status code or failed to receive body
-    NotOk
+    NotOk,
 }
 
 /// Streams `CrawlerStreamResult`s for successful and failed crawljobs
@@ -84,7 +98,7 @@ pub struct CrawlerStream<T: Fn(&Url) -> CrawlJob> {
     client: T,
     settings: CrawlerSettings,
     scheduled: std::collections::HashSet<url::Url>,
-    running: Vec<(Url, CrawlJob)>
+    running: Vec<(Url, CrawlJob)>,
 }
 
 impl<T: Fn(&Url) -> CrawlJob + Unpin> Stream for CrawlerStream<T> {
@@ -95,26 +109,30 @@ impl<T: Fn(&Url) -> CrawlJob + Unpin> Stream for CrawlerStream<T> {
             log::trace!("Finished with urls count: '{:?}'", self.scheduled.len());
             return Poll::Ready(None);
         }
-        
+
         let concurrent = self.settings.concurrent_requests;
-        let item = self.running.iter_mut().take(concurrent).enumerate().find_map(|(i, f)| {
-            match f.1.poll_unpin(cx) {
+        let item = self
+            .running
+            .iter_mut()
+            .take(concurrent)
+            .enumerate()
+            .find_map(|(i, f)| match f.1.poll_unpin(cx) {
                 Poll::Pending => None,
-                Poll::Ready(e) => Some((i, e))
-            } 
-        });
-        
-        if let Some((idx, result)) = item { 
+                Poll::Ready(e) => Some((i, e)),
+            });
+
+        if let Some((idx, result)) = item {
             self.running.swap_remove(idx).0;
             let links = &result.crawl_candidates;
             log::trace!(
-                "Suggested links (total: {}): {:?},",  
+                "Suggested links (total: {}): {:?},",
                 links.len(),
-                links.iter().take(10).map(|url| url.to_string()));
+                links.iter().take(10).map(|url| url.to_string())
+            );
             self.schedule(links);
-            return Poll::Ready(Some(result))
-        } 
-        Poll::Pending        
+            return Poll::Ready(Some(result));
+        }
+        Poll::Pending
     }
 }
 
@@ -127,15 +145,15 @@ impl<T: Fn(&Url) -> CrawlJob> CrawlerStream<T> {
         let future = (client)(&start_url);
         let mut scheduled = HashSet::new();
         scheduled.insert(start_url.clone());
-        
+
         Self {
             client,
             settings,
             scheduled,
-            running: vec![(start_url, future)]
+            running: vec![(start_url, future)],
         }
     }
-    
+
     /// Schedules urls which are not scheduled yet until settings.page_limit is reached
     fn schedule(&mut self, links: &Vec<Url>) {
         log::trace!("Schedule '{}' items", links.len());
@@ -155,17 +173,17 @@ impl<T: Fn(&Url) -> CrawlJob> CrawlerStream<T> {
 
 pub struct CrawlerSettings {
     /// Number of pages to parse. This number includes pages which produced an error on crawling.
-    /// This setting should prevent the user from creating infinitely many requests, as 
+    /// This setting should prevent the user from creating infinitely many requests, as
     /// CrawlerStream has to maintain a HashSet of visited urls which could otherwise overflow the memory.
     /// use `future::Stream::take()` with a sufficient page_limit if you want an exact number of successful results.
     page_limit: usize,
 
-    /// Maximal timeout until a request is cancelled. The crawler still continues parsing 
+    /// Maximal timeout until a request is cancelled. The crawler still continues parsing
     page_timeout: std::time::Duration,
 
     /// Number of concurrently open requests to the crawl target. IO is done on the awaiting thread,
     /// parsing uses the `actix_web::web::block()` threadpool. CrawlerStream doesn't spawn any new threads.
-    concurrent_requests: usize
+    concurrent_requests: usize,
 }
 
 impl Default for CrawlerSettings {
@@ -173,7 +191,7 @@ impl Default for CrawlerSettings {
         CrawlerSettings {
             page_limit: 5000,
             page_timeout: std::time::Duration::from_secs(1),
-            concurrent_requests: 50
+            concurrent_requests: 50,
         }
     }
 }
@@ -182,13 +200,14 @@ fn parse(url: &Url, body: &Bytes) -> ParserResult {
     let str = String::from_utf8_lossy(body.bytes());
     let fragment = Html::parse_document(str.as_ref());
     let selector = Selector::parse("a[href]").unwrap();
-    let interesting_links = fragment.select(&selector)
-        .filter_map(|link_el| {   
+    let interesting_links = fragment
+        .select(&selector)
+        .filter_map(|link_el| {
             let base = Url::options().base_url(Some(url));
-            base.parse(link_el.value().attr("href").unwrap()).ok()            
+            base.parse(link_el.value().attr("href").unwrap()).ok()
         })
         .filter(|a| a.domain() == url.domain() && a.scheme().starts_with("http"));
-    
+
     let mut effectual_candidates = Vec::new();
     let mut problems = Vec::new();
 
@@ -196,95 +215,105 @@ fn parse(url: &Url, body: &Bytes) -> ParserResult {
         if url.scheme() == "http" {
             problems.push(PageProblem::InsecureLink { url });
         } else {
-            effectual_candidates.push(url); 
+            effectual_candidates.push(url);
         }
     }
-    ParserResult { crawl_candidates: effectual_candidates, problems }
+    ParserResult {
+        crawl_candidates: effectual_candidates,
+        problems,
+    }
 }
 
 /// Factory to create CrawlerStreams from urls
 pub struct CrawlerStreamFactory {
     client: actix_web::client::Client,
-    settings: CrawlerSettings
+    settings: CrawlerSettings,
 }
 
 impl CrawlerStreamFactory {
     pub fn new_rustls() -> Self {
         let mut config = rustls::ClientConfig::new();
-        config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+        config
+            .root_store
+            .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
         let client = actix_web::client::Client::builder()
-            .connector(actix_web::client::Connector::new().rustls(Arc::new(config)).finish())
+            .connector(
+                actix_web::client::Connector::new()
+                    .rustls(Arc::new(config))
+                    .finish(),
+            )
             .finish();
-        Self { client, settings: CrawlerSettings::default() }
+        Self {
+            client,
+            settings: CrawlerSettings::default(),
+        }
     }
 
-    /// Creates a new CrawlerStream for the provided URL 
+    /// Creates a new CrawlerStream for the provided URL
     pub fn create(&self, start_url: Url) -> CrawlerStream<impl Fn(&Url) -> CrawlJob> {
         let client = self.client.clone();
         let timeout = self.settings.page_timeout;
         CrawlerStream::new(
             move |url| {
-                let request = client
-                    .get(url.as_str())
-                    .timeout(timeout)
-                    .send();
+                let request = client.get(url.as_str()).timeout(timeout).send();
 
                 let parse_url = url.clone();
                 async move {
                     let mut response = match request.await {
                         Ok(r) => r,
-                        Err(_) => return CrawlerStreamResult::new_error(parse_url, None, vec![])
+                        Err(_) => return CrawlerStreamResult::new_error(parse_url, None, vec![]),
                     };
                     let status_code = response.status().as_u16();
                     if !response.status().is_success() && !response.status().is_redirection() {
                         return CrawlerStreamResult::new_error(
                             parse_url,
                             Some(status_code),
-                            vec![PageProblem::NotOk]
-                         );
+                            vec![PageProblem::NotOk],
+                        );
                     }
-                    
+
                     let body = match response.body().await {
                         Ok(r) => r,
                         Err(e) => {
                             return CrawlerStreamResult::new_error(
                                 parse_url,
                                 Some(status_code),
-                                if let PayloadError::Overflow = e { 
-                                    Vec::new() 
-                                } else { 
+                                if let PayloadError::Overflow = e {
+                                    Vec::new()
+                                } else {
                                     vec![PageProblem::NotOk]
-                                }
+                                },
                             )
                         }
                     };
                     let result_url = parse_url.clone();
-                    let parse_result = actix_web::web::block::<_,_,()>(move || { 
-                        Ok(parse(&parse_url, &body))
-                    }).await.unwrap();
-            
+                    let parse_result =
+                        actix_web::web::block::<_, _, ()>(move || Ok(parse(&parse_url, &body)))
+                            .await
+                            .unwrap();
+
                     CrawlerStreamResult {
                         url: result_url,
                         status_code: Some(status_code),
                         crawl_candidates: parse_result.crawl_candidates,
                         problems: parse_result.problems,
-                        make_ctor_private: PhantomData
+                        make_ctor_private: PhantomData,
                     }
-                }.boxed_local()
-            }, 
-            start_url)
-    }   
+                }
+                .boxed_local()
+            },
+            start_url,
+        )
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use actix_web::web::{Buf, BufMut, BytesMut};
     use {super::*, std::collections::HashMap};
-    
-    
+
     #[actix_rt::test]
     async fn resolve_recursive_once() {
-        
         let home_url = Url::parse("https://foo.ch").unwrap();
         let home_with_slash = Url::parse("https://foo.ch/").unwrap();
         let bar_url = Url::parse("https://foo.ch/bar").unwrap();
@@ -300,20 +329,27 @@ mod tests {
                     status_code: Some(200),
                     crawl_candidates: map.get(url).unwrap().clone(),
                     problems: Vec::new(),
-                    make_ctor_private: PhantomData                    
+                    make_ctor_private: PhantomData,
                 };
                 async move { result }.boxed_local()
-            }, 
-            home_url);
+            },
+            home_url,
+        );
 
         let all = crawler.collect::<Vec<_>>().await;
-        
+
         assert_eq!(2, all.len());
     }
 
     #[test]
     fn parse_supports_linktypes() {
-        for source in ["https://mineichen.ch/home/next.php", "/home/next.php", "next.php"].iter() {
+        for source in [
+            "https://mineichen.ch/home/next.php",
+            "/home/next.php",
+            "next.php",
+        ]
+        .iter()
+        {
             let source_url = Url::parse("https://mineichen.ch/home/current.php?test=1").unwrap();
             let expected_url = Url::parse("https://mineichen.ch/home/next.php").unwrap();
             let content = build_page_with_links(std::iter::once(*source));
@@ -330,7 +366,7 @@ mod tests {
             let content = build_page_with_links(std::iter::once(*source));
             let mut results = parse(&source_url, &content).crawl_candidates.into_iter();
             assert_eq!(None, results.next());
-        }       
+        }
     }
 
     #[test]
@@ -343,22 +379,24 @@ mod tests {
         let content = build_page_with_links(vec![valid1, http, valid2]);
         let mut result = parse(&source_url, &content);
         assert_eq!(2, result.crawl_candidates.len());
-        
+
         assert_eq!(
-            vec!(PageProblem::InsecureLink { url: Url::parse(http).unwrap()}),
+            vec!(PageProblem::InsecureLink {
+                url: Url::parse(http).unwrap()
+            }),
             result.problems
-        );    
+        );
     }
 
-    fn build_page_with_links(links: impl IntoIterator<Item=&'static str>) -> Bytes {
+    fn build_page_with_links(links: impl IntoIterator<Item = &'static str>) -> Bytes {
         let mut data = BytesMut::new();
         data.put(&b"<html><head></head><body>"[..]);
         for link in links {
             data.put(&b"<a href=\""[..]);
-            data.put(link.as_bytes());            
+            data.put(link.as_bytes());
             data.put(&b"\">Link</a>"[..]);
         }
-            
+
         data.put(&b"</body></html>"[..]);
         data.to_bytes()
     }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-use std::fmt::Write;
-
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, web::{self, BufMut}};
-use env_logger::{Env};
-use futures::prelude::*;
+use {
+    actix_web::{
+        get,
+        middleware::Logger,
+        web::{self, BufMut},
+        App, HttpResponse, HttpServer, Responder,
+    },
+    env_logger::Env,
+    futures::prelude::*,
+    std::fmt::Write,
+};
 
 mod crawler;
 
 #[derive(serde::Serialize)]
 struct InvalidUrlError {
-    invalid_url: String
+    invalid_url: String,
 }
 
 #[get("/crawler/domain/{domain}/ok")]
 async fn ok(domain: web::Path<String>, state: web::Data<AppState>) -> impl Responder {
     let url = parse_url(domain.into_inner())?;
-    
-    HttpResponse::Ok().streaming::<_, actix_web::Error>(state
-        .crawler.create(url)
-        .filter(|v| future::ready(v.is_ok()))
-        .map(|visit| {
-            let mut buf = actix_web::web::BytesMut::new();
-            buf.put(visit.url.as_str().as_bytes());
-            buf.put_u8(b'\n');
-            Ok(buf.into())
-        })).await
-} 
 
-// Pretty nasty, but I haven't found a better solution for streaming json yet. 
+    HttpResponse::Ok()
+        .streaming::<_, actix_web::Error>(
+            state
+                .crawler
+                .create(url)
+                .filter(|v| future::ready(v.is_ok()))
+                .map(|visit| {
+                    let mut buf = actix_web::web::BytesMut::new();
+                    buf.put(visit.url.as_str().as_bytes());
+                    buf.put_u8(b'\n');
+                    Ok(buf.into())
+                }),
+        )
+        .await
+}
+
+// Pretty nasty, but I haven't found a better solution for streaming json yet.
 #[get("/crawler/domain/{domain}/problems")]
 async fn problems(domain: web::Path<String>, state: web::Data<AppState>) -> impl Responder {
     let url = parse_url(domain.into_inner())?;
@@ -34,7 +45,7 @@ async fn problems(domain: web::Path<String>, state: web::Data<AppState>) -> impl
     let element_stream = state
         .crawler.create(url)
         .filter(|v| future::ready(!v.problems.is_empty()))
-        .map(move |visit| {            
+        .map(move |visit| {
             let mut buf = actix_web::web::BytesMut::new();
             if is_first {
                 is_first = false
@@ -49,49 +60,60 @@ async fn problems(domain: web::Path<String>, state: web::Data<AppState>) -> impl
             } else {
                 log::error!("Failed to convert to JSON. Result for url {} is therefore not sent to the output", visit.url.as_str());
             }
-            
+
             Ok(buf.into())
         });
-    let start = stream::once(future::ready(Ok(actix_web::web::Bytes::from_static(&b"["[..]))));
-    let end = stream::once(future::ready(Ok(actix_web::web::Bytes::from_static(&b"]"[..]))));
-    HttpResponse::Ok().content_type("application/json").streaming::<_, actix_web::Error>(
-        start.chain(element_stream).chain(end)
-    ).await
-} 
+    let start = stream::once(future::ready(Ok(actix_web::web::Bytes::from_static(
+        &b"["[..],
+    ))));
+    let end = stream::once(future::ready(Ok(actix_web::web::Bytes::from_static(
+        &b"]"[..],
+    ))));
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .streaming::<_, actix_web::Error>(start.chain(element_stream).chain(end))
+        .await
+}
 
 #[get("/crawler/domain/{domain}/ok/count")]
-async fn count_ok(domain: web::Path<String>, state: web::Data<AppState>) -> Result<String, actix_web::Error> {
+async fn count_ok(
+    domain: web::Path<String>,
+    state: web::Data<AppState>,
+) -> Result<String, actix_web::Error> {
     let url = parse_url(domain.into_inner())?;
-    
-    Ok(state.crawler.create(url)
+
+    Ok(state
+        .crawler
+        .create(url)
         .filter(|v| future::ready(v.is_ok()))
         .fold(0, |acc, _x| async move { acc + 1 })
         .await
-        .to_string()
-    )
+        .to_string())
 }
 
 #[get("/crawler/domain/{domain}/problems/count")]
-async fn count_problems(domain: web::Path<String>, state: web::Data<AppState>) -> Result<String, actix_web::Error> {
+async fn count_problems(
+    domain: web::Path<String>,
+    state: web::Data<AppState>,
+) -> Result<String, actix_web::Error> {
     let url = parse_url(domain.into_inner())?;
-    
-    Ok(state.crawler.create(url)
+
+    Ok(state
+        .crawler
+        .create(url)
         .fold(0, |acc, x| async move { acc + x.problems.len() })
         .await
-        .to_string()
-    )
+        .to_string())
 }
-
 
 fn parse_url(mut input: String) -> Result<url::Url, actix_web::Error> {
     input.insert_str(0, "https://"); // yes, https only unless PO requests otherwise :-)
     Ok(url::Url::parse(&input)
-        .map_err(|_| HttpResponse::BadRequest()
-            .json(InvalidUrlError { invalid_url: input }))?)
+        .map_err(|_| HttpResponse::BadRequest().json(InvalidUrlError { invalid_url: input }))?)
 }
 
 struct AppState {
-    pub crawler: crawler::CrawlerStreamFactory
+    pub crawler: crawler::CrawlerStreamFactory,
 }
 
 #[actix_web::main]
@@ -108,7 +130,7 @@ async fn main() -> std::io::Result<()> {
             .service(count_ok)
             .service(problems)
             .service(count_problems)
-    })    
+    })
     .bind("0.0.0.0:8080")?
     .run()
     .await
@@ -118,32 +140,41 @@ mod tests {
     use super::*;
     use actix_web::{test, App};
 
-
     #[actix_rt::test]
-    async fn request_invalid_url_returns_400_status() {    
-        let mut app = test::init_service(App::new()
-            .data(AppState {
-                crawler: crawler::CrawlerStreamFactory::new_rustls(),
-            })        
-            .service(ok)
-            .service(count_ok)
-            .service(problems)
-            .service(count_problems)
-        ).await;  
-        
-        let req = test::TestRequest::get().uri("/crawler/domain/i%nvalid/ok").to_request();
+    async fn request_invalid_url_returns_400_status() {
+        let mut app = test::init_service(
+            App::new()
+                .data(AppState {
+                    crawler: crawler::CrawlerStreamFactory::new_rustls(),
+                })
+                .service(ok)
+                .service(count_ok)
+                .service(problems)
+                .service(count_problems),
+        )
+        .await;
+
+        let req = test::TestRequest::get()
+            .uri("/crawler/domain/i%nvalid/ok")
+            .to_request();
         let resp = test::call_service(&mut app, req).await;
         assert_eq!(400, resp.status());
 
-        let req = test::TestRequest::get().uri("/crawler/domain/i%nvalid/ok/count").to_request();
+        let req = test::TestRequest::get()
+            .uri("/crawler/domain/i%nvalid/ok/count")
+            .to_request();
         let resp = test::call_service(&mut app, req).await;
         assert_eq!(400, resp.status());
 
-        let req = test::TestRequest::get().uri("/crawler/domain/i%nvalid/problems").to_request();
+        let req = test::TestRequest::get()
+            .uri("/crawler/domain/i%nvalid/problems")
+            .to_request();
         let resp = test::call_service(&mut app, req).await;
         assert_eq!(400, resp.status());
 
-        let req = test::TestRequest::get().uri("/crawler/domain/i%nvalid/problems/count").to_request();
+        let req = test::TestRequest::get()
+            .uri("/crawler/domain/i%nvalid/problems/count")
+            .to_request();
         let resp = test::call_service(&mut app, req).await;
         assert_eq!(400, resp.status());
     }
